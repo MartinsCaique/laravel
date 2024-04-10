496264c9b72ebcfe4c8a292c1911a3dfe3ccfd1d
MartinsCaique <caiquety1@gmail.com>
2024-04-10 18:10:46 -0300

    Commit

--- resources/views/site/fornecedores.blade.php
+++ resources/views/site/fornecedores.blade.php
@@ -7,5 +7,15 @@
 </head>
 <body>
     <h1>Fornecedores</h1>
+    
+    {{-- @dd($fornecedores) --}}
+
+    @if (count($fornecedores) > 0 && count($fornecedores) < 10)
+        <h3>Há alguns fornecedores cadastrados</h3>
+    @elseif (count($fornecedore) > 10)
+        <h3>Há muitos fornecedores cadastrados</h3>
+    @else
+        <h3>Não há fornecedores cadastrados/h3>
+    @endif
 </body>
 </html>
