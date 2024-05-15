d850f33489963e1ce38c9d1fc87551fe76504023
MartinsCaique <caiquety1@gmail.com>
2024-05-15 17:44:34 -0300

    pronto

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -7,32 +7,10 @@
     <div class="container">
         <div class="row d-flex justify-content-center">
             <div class="col-6">
-                <form action="{{ route('site.contato') }}" method="GET">
-                    <div class="mb-3">
-                        <label for="nome" class="form-label">Nome Completo</label>
-                        <input type="text" id="nome" class="form-control" name="nome">
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="email" class="form-label">E-mail</label>
-                        <input type="text" id="email" class="form-control" name="email">
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="mensagem" class="form-label">Mensagem</label>
-                        <input type="text" id="mensagem" class="form-control" name="mensagem">
-                    </div>
-
-                    <div class="mb-3">
-                        <select name="motivo" id="motivo" class="form-select">
-                            <option selected disabled>Qual o motivo do contato?</option>
-                            <option value="1">Reclamação</option>
-                            <option value="2">Dúvida</option>
-                            <option value="3">Elegios</option>
-                        </select>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Enviar</button>
-                </form>
+                @component('site.layouts._components.form_contato', ['dark' => 'bg-light'])
+                @endcomponent
+                <p>A nossa equipe analisará o caso e entraremos em contato!</p>
+                <p>Tempo de resposta em 48 horas!</p>
             </div>
         </div>
     </div>
--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -2,6 +2,16 @@
 
 @section('titulo', 'Página Principal')
 
-@section('conteudo')
+    @section('conteudo')
     @include('site.layouts._partials.menu')
+
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-6">
+                @component('site.layouts._components.form_contato', ['dark' => 'bg-light'])
+                @endcomponent
+            </div>
+        </div>
+    </div>
+
 @endsection
