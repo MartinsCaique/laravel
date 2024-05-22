006a4c581c9e93aca3b4da7d280ed26fd24e8430
MartinsCaique <caiquety1@gmail.com>
2024-05-22 18:12:39 -0300

    DDD

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -6,7 +6,7 @@
     @include('site.layouts._partials.menu')
 
     <div class="container">
-        <div class="row d-flex justify-content-center">
+        <div class="nav-bar row d-flex justify-content-center">
             <div class="col-6">
                 @component('site.layouts._components.form_contato', ['dark' => 'bg-light'])
                 @endcomponent
