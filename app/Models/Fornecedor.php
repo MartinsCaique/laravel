d7f36ced1c1061c97d9c27068c86e99e2d2ebdc0
MartinsCaique <caiquety1@gmail.com>
2024-08-28 18:15:35 -0300

    Fornecedor com fillable

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -8,5 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Fornecedor extends Model
 {
     protected $table = 'fornecedores';
+    protected $fillable = [
+        'nome',
+        'site',
+        'uf',
+        'email',
+    ];
     use HasFactory;
 }
